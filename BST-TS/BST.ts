661f02fa06faf8254fb02505e59145357bdef7d7
Sam Gomez Olvera <samyolvera1@gmail.com>
2019-05-25 19:07:21 -0400

    refractored repository name files and code for interface to include union typing

---
+++ BST-TS/BST.ts
@@ -0,0 +1,46 @@
+import {IBinaryTree} from "./IBinaryTree";
+import {BST_Node} from "./BST_Node";
+
+
+
+class BST implements IBinaryTree
+{
+    contains(s: string): boolean {
+        return false;
+    }
+
+    delete(s: string): boolean {
+        return false;
+    }
+
+    empty(): boolean {
+        return false;
+    }
+
+    findMax(): string {
+        return "";
+    }
+
+    findMin(): string {
+        return "";
+    }
+
+    getRoot(): BST_Node {
+        return undefined;
+    }
+
+    height(): number {
+        return 0;
+    }
+
+    insert(s: string): boolean {
+        return false;
+    }
+
+    size(): number {
+        return 0;
+    }
+
+
+
+}
---
+++ BST-TS/BST_Node.ts
@@ -0,0 +1,8 @@
+export class BST_Node{
+    _root: BST_Node;
+    _left: BST_Node;
+    _right: BST_Node;
+    _data: string|number;
+
+
+}
